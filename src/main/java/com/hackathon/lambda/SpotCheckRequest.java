a1a717376081bdb95ba7c6b693e740d9b04ff172
sanjay <sjay@rams.colostate.edu>
2016-11-28 16:34:25 -0700

    landscapeEnum

--- src/main/java/com/hackathon/lambda/SpotCheckRequest.java
+++ src/main/java/com/hackathon/lambda/SpotCheckRequest.java
@@ -6,5 +6,50 @@ public class SpotCheckRequest {
 	Double lat;
 	Double lon;
 	CountryEnum country;
+	LandscapeEnum landscape;
 	Column column;
+	
+	public SpotCheckRequest(){}
+
+	public Double getLat() {
+		return lat;
+	}
+
+	public void setLat(Double lat) {
+		this.lat = lat;
+	}
+
+	public Double getLon() {
+		return lon;
+	}
+
+	public void setLon(Double lon) {
+		this.lon = lon;
+	}
+
+	public CountryEnum getCountry() {
+		return country;
+	}
+
+	public void setCountry(CountryEnum country) {
+		this.country = country;
+	}
+
+	public LandscapeEnum getLandscape() {
+		return landscape;
+	}
+
+	public void setLandscape(LandscapeEnum landscape) {
+		this.landscape = landscape;
+	}
+
+	public Column getColumn() {
+		return column;
+	}
+
+	public void setColumn(Column column) {
+		this.column = column;
+	}
+	
+	
 }
